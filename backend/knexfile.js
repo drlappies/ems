c7f025456cfe43951e3a8e907e9a7015e9eec4eb
drlappies <yau1chun2lap3@gmail.com>
2021-08-04 18:47:55 +0800

    first commit

---
+++ backend/knexfile.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const config = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
+
+module.exports = config
