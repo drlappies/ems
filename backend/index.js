d31544435fd0049b95c5eeba89b5a4373a63c663
drlappies <yau1chun2lap3@gmail.com>
2021-08-05 22:40:01 +0800

    unused user service controller

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,6 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-const { UserRoute } = require('./routes/user.route');
-const UserController = require('./controllers/user.controller');
-const UserService = require('./services/user.service');
-
-const userService = new UserService(knex)
-const userController = new UserController(userService);
-const userRoute = UserRoute(userController);
-app.use('/user', userRoute);
-
 const { EmployeeRoute } = require('./routes/employee.route');
 const EmployeeController = require('./controllers/employee.controller');
 const EmployeeService = require('./services/employee.service');
