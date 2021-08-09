f73211c2f134f2188f380c5443fb7a6e85c18b0b
drlappies <yau1chun2lap3@gmail.com>
2021-08-10 01:35:15 +0800

    create allowance employee junction table

--- backend/migrations/20210809104224_create-allowance-employee-tabl.js
+++ backend/migrations/20210809104224_create-allowance-employee-tabl.js
@@ -4,9 +4,9 @@ exports.up = async function (knex) {
     if (!table) {
         return knex.schema.createTable('allowance_employee', table => {
             table.integer('allowance_id')
-            table.foreign('allowance_id').references('allowance.id').deferrable('deferred')
+            table.foreign('allowance_id').references('allowance.id').onUpdate('CASCADE').onDelete('CASCADE')
             table.integer('employee_id')
-            table.foreign('employee_id').references('employee.id').deferrable('deferred')
+            table.foreign('employee_id').references('employee.id').onUpdate('CASCADE').onDelete('CASCADE')
         })
     }
 };
