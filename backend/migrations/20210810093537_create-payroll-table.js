c7ad5ab035158605a5cd5078195d61b59c4ef23a
drlappies <yau1chun2lap3@gmail.com>
2021-08-10 17:53:40 +0800

    create payroll table

---
+++ backend/migrations/20210810093537_create-payroll-table.js
@@ -0,0 +1,23 @@
+
+exports.up = async function (knex) {
+    const table = await knex.schema.hasTable('payroll');
+    if (!table) {
+        return knex.schema.createTable('payroll', table => {
+            table.increments('id');
+            table.integer('employee_id').unsigned().notNullable();
+            table.foreign('employee_id').references('employee.id');
+            table.date('from').notNullable();
+            table.date('to').notNullable();
+            table.decimal('amount', 8, 2).notNullable();
+            table.decimal('reimbursement', 8, 2).notNullable();
+            table.decimal('allowance', 8, 2).notNullable();
+            table.decimal('deduction', 8, 2).notNullable();
+            table.decimal('bonus', 8, 2).notNullable();
+            table.enu('status', ['pending', 'confirmed']).defaultTo('pending')
+        })
+    }
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable('payroll');
+};
