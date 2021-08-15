72d9b0c8da63e4ac992061740522ec095ee4711b
drlappies <yau1chun2lap3@gmail.com>
2021-08-16 02:55:37 +0800

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Home from './components/Home'
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
+  const auth = useSelector(state => state.auth.isAuthenticated)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Router>
+      {!auth ?
+        <Route exact path="/" component={Login} />
+        :
+        <Navbar>
+          <Switch>
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        </Navbar>
+      }
+    </Router>
   );
 }
 
