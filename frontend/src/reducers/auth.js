75365c0dd8daf8b2b7b0196c4f61c34fdb070848
drlappies <yau1chun2lap3@gmail.com>
2021-08-16 03:24:29 +0800

    auth reducer

---
+++ frontend/src/reducers/auth.js
@@ -0,0 +1,35 @@
+const initialState = {
+    isAuthenticated: false,
+    username: '',
+    firstname: '',
+    lastname: '',
+    role: '',
+    ot_entitled: false
+}
+
+const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {
+                username: action.payload.username,
+                firstname: action.payload.firstname,
+                lastname: action.payload.lastname,
+                role: action.payload.role,
+                ot_entitled: action.payload.role,
+                isAuthenticated: true
+            }
+        case 'LOGOUT':
+            return {
+                username: '',
+                firstname: '',
+                lastname: '',
+                role: '',
+                ot_entitled: '',
+                isAuthenticated: false
+            }
+        default:
+            return state;
+    }
+}
+
+export default authReducer
