785c07ad9907bd80838df6b7e22a21268d0e2269
drlappies <yau1chun2lap3@gmail.com>
2021-08-16 03:25:29 +0800

    login persistance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { verifyThunk } from './actions/auth';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
+  const dispatch = useDispatch()
   const auth = useSelector(state => state.auth.isAuthenticated)
+
+  useEffect(() => {
+    dispatch(verifyThunk(window.localStorage.getItem('jwt')))
+  }, [dispatch])
 
   return (
     <Router>
