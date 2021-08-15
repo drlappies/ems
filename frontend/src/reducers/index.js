7dc2df80abf60432b4a1b07487594bd0e983911e
drlappies <yau1chun2lap3@gmail.com>
2021-08-16 03:24:55 +0800

    combined reducers

---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import authReducer from "./auth";
+import uiReducer from './ui';
+
+const reducer = combineReducers({
+    auth: authReducer,
+    ui: uiReducer
+})
+
+export default reducer
