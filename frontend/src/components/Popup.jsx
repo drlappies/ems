01577fc81f067c5bf205ca37c5154b3d3c49eace
drlappies <yau1chun2lap3@gmail.com>
2021-08-16 03:25:48 +0800

    alert

---
+++ frontend/src/components/Popup.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Message } from 'semantic-ui-react'
+import { popMessage } from '../actions/ui'
+
+function Popup(props) {
+    const dispatch = useDispatch()
+    const notification = useSelector(state => state.ui)
+
+    let isSuccess = false;
+    let isError = false;
+    if (notification.type === 'success') {
+        isSuccess = true
+    } else if (notification.type === 'error') {
+        isError = true
+    }
+
+    return (
+        <Message hidden={!notification.visible} success={isSuccess} error={isError} onDismiss={() => dispatch(popMessage('', 'DISMISS'))}>
+            <Message.Content>
+                {notification.message}
+            </Message.Content>
+        </Message>
+    )
+}
+
+export default Popup
