9b3eea731480c897cef61bf263b01b3c38b9162f
drlappies <yau1chun2lap3@gmail.com>
2021-08-17 05:01:12 +0800

    attendance ui;

---
+++ frontend/src/components/Attendance.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Grid } from 'semantic-ui-react'
+import Punch from './Punch'
+import Popup from './Popup';
+import '../App.css'
+
+function Attendance() {
+    return (
+        <Container>
+            <Grid centered>
+                <Grid.Row>
+                    <Popup />
+                </Grid.Row>
+                <Grid.Row>
+                    <Punch />
+                </Grid.Row>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Attendance
