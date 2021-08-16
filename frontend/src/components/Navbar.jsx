1a7a6e9e84266a505028141dcf07e6cb403215df
drlappies <yau1chun2lap3@gmail.com>
2021-08-17 05:05:56 +0800

    sidenar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Icon, Menu, Sidebar, Container } from 'semantic-ui-react'
+
+function Navbar(props) {
+    const ui = useSelector(state => state.ui)
+
+    return (
+        <React.Fragment>
+            <Sidebar.Pushable style={{ height: '100vh' }}>
+                <Sidebar
+                    as={Menu}
+                    animation="overlay"
+                    inverted
+                    vertical
+                    visible={ui.sidebar}
+                    width='wide'
+                >
+                    <Menu.Item>
+                        <Menu.Header>
+                            <Icon name='dashboard' />Dashboard
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item as={Link} to="/dashboard">
+                                Dashboard
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Menu.Header>
+                            <Icon name="time" />Attendance Management
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item as={Link} to="/attendance/time">
+                                Employee Time In
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Attendance History
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Menu.Header>
+                            <Icon name="money" />Payroll Management
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item as={Link}>
+                                Generate Payroll
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Payroll History
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Menu.Header>
+                            <Icon name="users" />HR Management
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item as={Link}>
+                                Employee
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Position
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Department
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Menu.Header>
+                            <Icon name="money bill alternate outline" />Allowance Management
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item as={Link}>
+                                View Allowance
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Create Allowance
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Employee - Allowance Management
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Menu.Header>
+                            <Icon name="money bill alternate" />Reimbursement Management
+                        </Menu.Header>
+                        <Menu.Menu>
+                            <Menu.Item as={Link}>
+                                Reimbursement Application
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Reimbursement Approval
+                            </Menu.Item>
+                            <Menu.Item as={Link}>
+                                Reimbursement History
+                            </Menu.Item>
+                        </Menu.Menu>
+                    </Menu.Item>
+                </Sidebar>
+                <Sidebar.Pusher dimmed={ui.sidebar}>
+                    <Container>
+                        {props.children}
+                    </Container>
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
+        </React.Fragment>
+    )
+}
+
+export default Navbar
