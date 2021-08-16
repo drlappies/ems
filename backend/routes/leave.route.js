e6350c60e64fe4930d5ce897fa42f2526b9271f4
drlappies <yau1chun2lap3@gmail.com>
2021-08-17 05:03:16 +0800

    leave rate metric route;

--- backend/routes/leave.route.js
+++ backend/routes/leave.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 module.exports.LeaveRoute = (LeaveController) => {
     const router = express.Router();
+    router.get('/rate', LeaveController.leaveRate)
     router.get('/', LeaveController.getAllLeave)
     router.get('/:id', LeaveController.getLeave)
     router.post('/', LeaveController.createLeave)
