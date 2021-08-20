9e54a05d7b6bee5728c38f4065bcb33766d7ee34
drlappies <yau1chun2lap3@gmail.com>
2021-08-20 20:26:52 +0800

    unuse

--- backend/routes/overtime.route.js
+++ backend/routes/overtime.route.js
@@ -7,8 +7,6 @@ module.exports.OvertimeRoute = (OvertimeController) => {
     router.post('/', OvertimeController.createOvertime);
     router.post('/timein', OvertimeController.createOvertimeTimein);
     router.post('/timeout', OvertimeController.createOvertimeTimeout);
-    router.post('/re-timein', OvertimeController.recreateOvertimeTimein);
-    router.post('/re-timeout', OvertimeController.recreateOvertimeTimeout);
     router.put('/:id', OvertimeController.updateOvertime);
     router.delete('/:id', OvertimeController.deleteOvertime);
     return router
