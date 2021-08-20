ec9c9c7e7f1f7cdf9cbf2bf12e6bbaa4f7c85213
drlappies <yau1chun2lap3@gmail.com>
2021-08-20 20:26:40 +0800

    populate create result

--- backend/controllers/overtime.controller.js
+++ backend/controllers/overtime.controller.js
@@ -59,7 +59,7 @@ class OvertimeController {
 
     createOvertime = async (req, res) => {
         try {
-            const { employee_id, date, from, to } = req.body;
+            const { employee_id, date, from, to, status } = req.body;
             if (!employee_id || !date || !from || !to) {
                 return res.status(400).json({
                     error: 'Missing required fields.'
@@ -71,15 +71,15 @@ class OvertimeController {
                     error: 'Cannot time in future dates.'
                 })
             }
-            const isTimedIn = await this.OvertimeService.checkIfSpecificTimedin(employee_id, date)
-            if (isTimedIn.length >= 1) {
+            if (await this.OvertimeService.checkForConflict(employee_id, date)) {
                 return res.status(400).json({
-                    error: 'Already timed in that day.'
+                    error: 'Already timed in that day'
                 })
             }
-            const overtime = await this.OvertimeService.createOvertime(employee_id, date, from, to)
+            const overtime = await this.OvertimeService.createOvertime(employee_id, date, from, to, status)
             return res.status(200).json({
-                success: `Succsesfully created specific overtime record on ${overtime.date.getFullYear()}-${overtime.date.getMonth() + 1}-${overtime.date.getDate()}`
+                success: `Succsesfully created specific overtime record on ${overtime[0].date.getFullYear()}-${overtime[0].date.getMonth() + 1}-${overtime[0].date.getDate()}`,
+                overtime: overtime
             })
         } catch (err) {
             console.log(err)
@@ -122,64 +122,6 @@ class OvertimeController {
             const overtime = await this.OvertimeService.deleteOvertime(id);
             return res.status(200).json({
                 success: `Successfully deleted overtime record ${overtime.id}`
-            })
-        } catch (err) {
-            console.log(err)
-            return res.status(500).json({
-                error: err
-            })
-        }
-    }
-
-    recreateOvertimeTimein = async (req, res) => {
-        try {
-            const { employee_id, date, from } = req.body;
-            if (!employee_id || !date || !from) {
-                return res.status(400).json({
-                    error: 'Missing required fields.'
-                })
-            }
-            const currentDate = new Date(new Date().setHours(0, 0, 0, 0))
-            if (new Date(date) > currentDate) {
-                return res.status(400).json({
-                    error: 'Cannot time in future dates.'
-                })
-            }
-            const isTimedin = await this.OvertimeService.checkIfSpecificTimedin(employee_id, date);
-            if (isTimedin.length >= 1) {
-                return res.status(400).json({
-                    error: 'Already timed in that day.'
-                })
-            }
-            const timein = await this.OvertimeService.recreateOvertimeTimein(employee_id, date, from);
-            return res.status(200).json({
-                success: `Successfully re-timed in for ${timein.date.getFullYear()}-${timein.date.getMonth()}-${timein.date.getDate()}.`
-            })
-        } catch (err) {
-            console.log(err);
-            return res.status(500).json({
-                error: err
-            })
-        }
-    }
-
-    recreateOvertimeTimeout = async (req, res) => {
-        try {
-            const { employee_id, date, to } = req.body;
-            if (!employee_id || !date || !to) {
-                return res.status(400).json({
-                    error: 'Missing required fields.'
-                })
-            }
-            const isTimedout = await this.OvertimeService.checkIfSpecificTimedout(employee_id, date)
-            if (isTimedout.length >= 1) {
-                return res.status(400).json({
-                    error: 'Already timed out that day.'
-                })
-            }
-            const timeout = await this.OvertimeService.recreateOvertimeTimeout(employee_id, date, to);
-            return res.status(200).json({
-                success: `Successfully re-timed out for ${timeout.date.getFullYear()}-${timeout.date.getMonth() + 1}-${timeout.date.getDate()} at ${timeout.to}`
             })
         } catch (err) {
             console.log(err)
