bac398f262b4a87574fedae674f0a07a94687a9a
drlappies <yau1chun2lap3@gmail.com>
2021-08-23 15:54:26 +0800

    employee redux

--- frontend/src/actions/employee.js
+++ frontend/src/actions/employee.js
@@ -1,14 +1,135 @@
-import { FETCH_EMPLOYEE } from '../types/employee'
+import { CONFIRM_EMPLOYEE_UPDATE, FETCH_EMPLOYEE, FETCH_SPECIFIC_EMPLOYEE, UPDATE_SPECIFIC_EMPLOYEE, DELETE_EMPLOYEE } from '../types/employee'
 import axios from 'axios';
 
 export const fetchEmployee = () => {
     return async (dispatch) => {
-        const res = await axios.get('/employee');
-        dispatch({
-            type: FETCH_EMPLOYEE,
-            payload: {
-                record: res.data.employee
+        try {
+            const res = await axios.get('/employee');
+            dispatch({
+                type: FETCH_EMPLOYEE,
+                payload: {
+                    record: res.data.employee
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export const fetchSpecificEmployee = (id) => {
+    return async (dispatch) => {
+        try {
+            const res = await axios.get(`/employee/${id}`)
+            dispatch({
+                type: FETCH_SPECIFIC_EMPLOYEE,
+                payload: {
+                    employeeId: res.data.employee.id,
+                    employeeFirstname: res.data.employee.firstname,
+                    employeeLastname: res.data.employee.lastname,
+                    employeeAddress: res.data.employee.address,
+                    employeePosition: res.data.employee.post,
+                    employeePositionId: res.data.employee.post_id,
+                    employeeDepartment: res.data.employee.name,
+                    employeeDepartmentId: res.data.employee.dept_id,
+                    employeeNumber: res.data.employee.phone_number,
+                    employeeContactPerson: res.data.employee.emergency_contact_person,
+                    employeeContactNumber: res.data.employee.emergency_contact_number,
+                    employeeOnboardDate: res.data.employee.onboard_date,
+                    employeeRole: res.data.employee.role,
+                    employeeSalary: res.data.employee.salary_monthly,
+                    employeeOT: res.data.employee.ot_pay_entitled,
+                    employeeOTpay: res.data.employee.ot_hourly_salary,
+                    employeeStatus: res.data.employee.status,
+                    employeeStartHour: res.data.employee.start_hour,
+                    employeeEndHour: res.data.employee.end_hour
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export const updateSpecificEmployee = (e) => {
+    return (dispatch) => {
+        try {
+            const name = e.target.name
+            const value = e.target.type === "checkbox" ? e.target.checked : e.target.value
+            dispatch({
+                type: UPDATE_SPECIFIC_EMPLOYEE,
+                payload: {
+                    name: name,
+                    value: value
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export const confirmEmployeeUpdate = (id, firstname, lastname, position, department, address, phoneNumber, contactPerson, contactNumber, onboardDate, status, isOtEntitled, otPay, startHour, endHour) => {
+    return async (dispatch) => {
+        try {
+            const body = {
+                department: department,
+                position: position,
+                firstname: firstname,
+                lastname: lastname,
+                address: address,
+                phone_number: phoneNumber,
+                emergency_contact_number: contactNumber,
+                emergency_contact_person: contactPerson,
+                onboard_date: onboardDate,
+                status: status,
+                ot_pay_entitled: isOtEntitled,
+                ot_hourly_salary: otPay,
+                starting: startHour,
+                ending: endHour
             }
-        })
+            const res = await axios.put(`/employee/${id}`, body)
+            dispatch({
+                type: CONFIRM_EMPLOYEE_UPDATE,
+                payload: {
+                    employeeId: res.data.employee.id,
+                    employeeFirstname: res.data.employee.firstname,
+                    employeeLastname: res.data.employee.lastname,
+                    employeeAddress: res.data.employee.address,
+                    employeePosition: res.data.employee.post,
+                    employeeDepartment: res.data.employee.name,
+                    employeeNumber: res.data.employee.phone_number,
+                    employeeContactPerson: res.data.employee.emergency_contact_person,
+                    employeeContactNumber: res.data.employee.emergency_contact_number,
+                    employeeOnboardDate: res.data.employee.onboard_date,
+                    employeeRole: res.data.employee.role,
+                    employeeSalary: res.data.employee.salary_monthly,
+                    employeeOT: res.data.employee.ot_pay_entitled,
+                    employeeOTpay: res.data.employee.ot_hourly_salary,
+                    employeeStatus: res.data.employee.status,
+                    employeeStartHour: res.data.employee.start_hour,
+                    employeeEndHour: res.data.employee.end_hour
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
+
+export const deleteEmployee = (employeeId) => {
+    return async (dispatch) => {
+        try {
+            const res = await axios.delete(`/employee/${employeeId}`)
+            console.log(res.data)
+            dispatch({
+                type: DELETE_EMPLOYEE,
+                payload: {
+                    employeeId: res.data.employee.id
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
--- frontend/src/reducers/employee.js
+++ frontend/src/reducers/employee.js
@@ -1,7 +1,26 @@
-import { FETCH_EMPLOYEE } from '../types/employee'
+import { FETCH_EMPLOYEE, FETCH_SPECIFIC_EMPLOYEE, UPDATE_SPECIFIC_EMPLOYEE, CONFIRM_EMPLOYEE_UPDATE, DELETE_EMPLOYEE } from '../types/employee'
 
 const initialState = {
     record: [],
+    employeeId: "",
+    employeeFirstname: "",
+    employeeLastname: "",
+    employeeAddress: "",
+    employeePosition: "",
+    employeePositionId: "",
+    employeeDepartment: "",
+    employeeDepartmentId: "",
+    employeeNumber: "",
+    employeeContactPerson: "",
+    employeeContactNumber: "",
+    employeeOnboardDate: "",
+    employeeRole: "",
+    employeeSalary: "",
+    employeeOT: false,
+    employeeOTpay: "",
+    employeeStatus: "",
+    employeeStartHour: "",
+    employeeEndHour: ""
 }
 
 const employeeReducer = (state = initialState, action) => {
@@ -9,7 +28,65 @@ const employeeReducer = (state = initialState, action) => {
         case FETCH_EMPLOYEE:
             return {
                 ...state,
-                record: action.payload.record
+                record: action.payload.record,
+            }
+        case FETCH_SPECIFIC_EMPLOYEE:
+            return {
+                ...state,
+                employeeId: action.payload.employeeId,
+                employeeFirstname: action.payload.employeeFirstname,
+                employeeLastname: action.payload.employeeLastname,
+                employeeAddress: action.payload.employeeAddress,
+                employeePosition: action.payload.employeePosition,
+                employeePositionId: action.payload.employeePositionId,
+                employeeDepartment: action.payload.employeeDepartment,
+                employeeDepartmentId: action.payload.employeeDepartmentId,
+                employeeNumber: action.payload.employeeNumber,
+                employeeContactPerson: action.payload.employeeContactPerson,
+                employeeContactNumber: action.payload.employeeContactNumber,
+                employeeOnboardDate: action.payload.employeeOnboardDate,
+                employeeRole: action.payload.employeeRole,
+                employeeSalary: action.payload.employeeSalary,
+                employeeOT: action.payload.employeeOT,
+                employeeOTpay: action.payload.employeeOTpay,
+                employeeStatus: action.payload.employeeStatus,
+                employeeStartHour: action.payload.employeeStartHour,
+                employeeEndHour: action.payload.employeeEndHour
+            }
+        case UPDATE_SPECIFIC_EMPLOYEE:
+            return {
+                ...state,
+                [action.payload.name]: action.payload.value
+            }
+        case CONFIRM_EMPLOYEE_UPDATE:
+            return {
+                ...state,
+                record: state.record.map(el =>
+                    el.id === action.payload.employeeId ? {
+                        ...el,
+                        firstname: action.payload.employeeFirstname,
+                        lastname: action.payload.employeeLastname,
+                        address: action.payload.employeeAddress,
+                        position: action.payload.employeePosition,
+                        department: action.payload.employeeDepartment,
+                        phone_number: action.payload.employeeNumber,
+                        emergency_contact_person: action.payload.employeeContactPerson,
+                        emergency_contact_number: action.payload.employeeContactNumber,
+                        onboard_date: action.payload.employeeOnboardDate,
+                        role: action.payload.employeeRole,
+                        status: action.payload.employeeStatus,
+                        start_hour: action.payload.employeeStartHour,
+                        end_hour: action.payload.employeeEndHour,
+                        salary_monthly: action.payload.employeeSalary,
+                        ot_pay_entitled: action.payload.employeeOT,
+                        ot_hourly_salary: action.payload.employeeOTpay,
+                    } : el
+                )
+            }
+        case DELETE_EMPLOYEE:
+            return {
+                ...state,
+                record: state.record.filter(el => el.id !== action.payload.employeeId)
             }
         default:
             return state
