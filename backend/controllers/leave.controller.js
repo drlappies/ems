e92aa10ae885d20f96ad4c8c211eee9b94218b07
drlappies <yau1chun2lap3@gmail.com>
2021-09-05 15:51:27 +0800

    leave history pagination andd query

--- backend/controllers/leave.controller.js
+++ backend/controllers/leave.controller.js
@@ -12,7 +12,9 @@ class LeaveController {
                 })
             }
             const leave = await this.LeaveService.getLeave(id);
-            return res.status(200).json(leave)
+            return res.status(200).json({
+                leave: leave
+            })
         } catch (err) {
             console.log(err)
             res.status(500).json({
@@ -23,8 +25,17 @@ class LeaveController {
 
     getAllLeave = async (req, res) => {
         try {
-            const leave = await this.LeaveService.getAllLeave();
-            return res.status(200).json(leave);
+            const { page, from, to, type, status, text, limit } = req.query
+            const data = await this.LeaveService.getAllLeave(page, from, to, type, status, text, limit);
+            return res.status(200).json({
+                leave: data.leave,
+                employee: data.employee,
+                currentPage: data.currentPage,
+                currentPageStart: data.currentPageStart,
+                currentPageEnd: data.currentPageEnd,
+                pageLength: data.pageLength,
+                currentLimit: data.currentLimit
+            });
         } catch (err) {
             console.log(err)
             res.status(500).json({
@@ -36,14 +47,22 @@ class LeaveController {
     createLeave = async (req, res) => {
         try {
             const { employeeId, reason, from, to, duration, type } = req.body;
-            if (!employeeId && !reason && !from && !to && !duration && !type) {
+            if (!employeeId || !reason || !from || !to || !duration || !type) {
                 return res.status(400).json({
                     error: 'Missing required fields.'
                 })
             }
+
+            if (await this.LeaveService.checkLeaveConflict(employeeId, from, to)) {
+                return res.status(400).json({
+                    error: 'Employee already has leave within this timespan.'
+                })
+            }
+
             const leave = await this.LeaveService.createLeave(employeeId, reason, from, to, duration, type);
+
             res.status(200).json({
-                success: `Successfully applied for leave from ${leave.from} to ${leave.to} for reason: ${leave.reason} type: ${leave.type} duration: ${leave.duration}`
+                success: `Successfully applied for leave from ${new Date(leave.from).getFullYear()} - ${new Date(leave.from).getMonth() + 1} - ${new Date(leave.from).getDate()} to ${new Date(leave.to).getFullYear()} - ${new Date(leave.to).getMonth() + 1} - ${new Date(leave.to).getDate()} Reason: ${leave.reason}`
             })
         } catch (err) {
             console.log(err)
@@ -53,24 +72,32 @@ class LeaveController {
         }
     }
 
-    approveLeave = async (req, res) => {
+    updateLeave = async (req, res) => {
         try {
-            const { id } = req.params;
-            const { status } = req.body;
-            if (!id && !status) {
-                return res.status(400).json({
-                    error: 'Missing required fields.'
-                })
-            }
-            const leave = await this.LeaveService.approveLeave(id, status);
+            let { status, duration, type, ids } = req.body;
+            if (!Array.isArray(ids)) ids = [ids];
+            const leave = await this.LeaveService.updateLeave(ids, duration, type, status);
             res.status(200).json({
-                success: `Successfully ${leave.status} leave id: ${leave.id}`
+                success: `Successfully updated leave record`
             })
         } catch (err) {
             console.log(err)
             res.status(500).json({
                 error: err
             })
+        }
+    }
+
+    deleteLeave = async (req, res) => {
+        try {
+            let { ids } = req.query;
+            if (!Array.isArray(ids)) ids = [ids];
+            const leave = await this.LeaveService.deleteLeave(ids)
+            res.status(200).json({
+                success: `Successfully deleted leave record`
+            })
+        } catch (err) {
+            console.log(err)
         }
     }
 
--- backend/services/leave.service.js
+++ backend/services/leave.service.js
@@ -6,16 +6,78 @@ class LeaveService {
     getLeave = async (id) => {
         const [leave] = await this.knex('leave')
             .join('employee', 'leave.employee_id', 'employee.id')
-            .select(['leave.id', 'leave.employee_id', 'leave.reason', 'leave.status', 'leave.duration', 'leave.from', 'leave.to', 'employee.firstname', 'employee.lastname'])
+            .select(['leave.id', 'leave.employee_id', 'leave.reason', 'leave.status', 'leave.duration', 'leave.from', 'leave.to', 'employee.firstname', 'employee.lastname', 'leave.type'])
             .where('leave.id', id)
         return leave
     }
 
-    getAllLeave = async () => {
+    getAllLeave = async (page, from, to, type, status, text, limit) => {
+        if (!page || page < 0) page = 0;
+        if (!limit) limit = 10;
+        let currentPage = parseInt(page)
+        let currentPageStart = parseInt(page) + 1
+        let currentPageEnd = parseInt(page) + parseInt(limit)
+        let currentLimit = parseInt(limit)
+
+        const employee = await this.knex('employee')
+            .select(['id', 'firstname', 'lastname'])
+
+        const [count] = await this.knex('leave')
+            .count()
+            .from(queryBuilder => {
+                queryBuilder
+                    .select(['employee.firstname', 'employee.lastname', 'leave.reason', 'leave.from', 'leave.to', 'leave.type', 'leave.status'])
+                    .from('leave')
+                    .join('employee', 'leave.employee_id', 'employee.id')
+                    .modify(queryBuilder => {
+                        if (from) {
+                            queryBuilder.where('leave.from', '>=', from)
+                        }
+                        if (to) {
+                            queryBuilder.where('leave.to', '<=', to)
+                        }
+                        if (type) {
+                            queryBuilder.where('leave.type', type)
+                        }
+                        if (text) {
+                            queryBuilder.whereRaw(`to_tsvector(employee.firstname || ' ' || employee.lastname || ' ' || leave.id || ' ' || leave.employee_id || ' ' || leave.reason) @@ plainto_tsquery('${text}')`)
+                        }
+                        if (status) {
+                            queryBuilder.where('leave.status', status)
+                        }
+                    })
+                    .as('count')
+            })
+
         const leave = await this.knex('leave')
             .join('employee', 'leave.employee_id', 'employee.id')
-            .select(['leave.id', 'leave.employee_id', 'leave.reason', 'leave.status', 'leave.duration', 'leave.from', 'leave.to', 'employee.firstname', 'employee.lastname'])
-        return leave
+            .select(['leave.id', 'leave.employee_id', 'employee.firstname', 'employee.lastname', 'leave.from', 'leave.to', 'leave.type', 'leave.status'])
+            .limit(currentLimit)
+            .offset(currentPage)
+            .orderBy('id')
+            .modify(queryBuilder => {
+                if (from) {
+                    queryBuilder.where('leave.from', '>=', from)
+                }
+                if (to) {
+                    queryBuilder.where('leave.to', '<=', to)
+                }
+                if (type) {
+                    queryBuilder.where('leave.type', type)
+                }
+                if (text) {
+                    queryBuilder.whereRaw(`to_tsvector(employee.firstname || ' ' || employee.lastname || ' ' || leave.id || ' ' || leave.employee_id || ' ' || leave.reason) @@ plainto_tsquery('${text}')`)
+                }
+                if (status) {
+                    queryBuilder.where('leave.status', status)
+                }
+            })
+
+        if (currentPageEnd >= count.count) {
+            currentPageEnd = parseInt(count.count)
+        }
+        console.log(page, from, to, type, status, text, limit)
+        return { leave: leave, employee: employee, currentPage: currentPage, currentPageStart: currentPageStart, currentPageEnd: currentPageEnd, pageLength: count.count, currentLimit: currentLimit }
     }
 
     createLeave = async (employeeId, reason, from, to, duration, type) => {
@@ -32,13 +94,42 @@ class LeaveService {
         return leave
     }
 
-    approveLeave = async (id, status) => {
-        const [leave] = await this.knex('leave').select()
-            .where('id', id)
-            .update({
-                status: status
-            }, ['id', 'status'])
+    updateLeave = async (ids, duration, type, status) => {
+        const update = {}
+        if (duration) update.duration = duration;
+        if (type) update.type = type;
+        if (status) update.status = status;
+
+        const [leave] = await this.knex('leave')
+            .select()
+            .whereIn('id', ids)
+            .update(update, ['id'])
         return leave
+    }
+
+    deleteLeave = async (ids) => {
+        const [leave] = await this.knex('leave')
+            .whereIn('id', ids)
+            .del(['id'])
+        return leave
+    }
+
+    checkLeaveConflict = async (employeeId, from, to) => {
+        const leave = await this.knex('leave')
+            .select('id')
+            .where('from', '>=', from)
+            .andWhere('to', '<=', to)
+            .andWhere('employee_id', employeeId)
+
+        return leave.length >= 1
+    }
+
+    checkAnnualLeave = async (employeeId) => {
+        const [employeeAL] = await this.knex('employee')
+            .select('annual_leave_count')
+            .where('id', employeeId)
+
+        return employeeAL
     }
 
     leaveRate = async () => {
