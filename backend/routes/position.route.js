c505fdbd7e4ee914c31e9d750979dc7639f0c750
drlappies <yau1chun2lap3@gmail.com>
2021-09-07 18:34:00 +0800

    add routes for batch update and delete

--- backend/routes/position.route.js
+++ backend/routes/position.route.js
@@ -3,9 +3,11 @@ const express = require('express')
 module.exports.PositionRoute = (positionController) => {
     const router = express.Router()
     router.get('/', positionController.getAllPosition)
+    router.get('/metric', positionController.getPositionCount)
     router.get('/:id', positionController.getPosition)
     router.post('/', positionController.createPosition)
     router.put('/:id', positionController.updatePosition)
+    router.delete('/', positionController.batchDeletePosition)
     router.delete('/:id', positionController.deletePosition)
     return router
 }
