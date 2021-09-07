5c3b61614e3367d613cf9488e20c541db7aed340
drlappies <yau1chun2lap3@gmail.com>
2021-09-08 00:50:13 +0800

    added controller for batch delete

--- backend/controllers/department.controller.js
+++ backend/controllers/department.controller.js
@@ -13,7 +13,9 @@ class DepartmentController {
             }
             const dept = await this.departmentService.getDepartment(id);
             return res.status(200).json({
-                dept: dept
+                id: dept.id,
+                name: dept.name,
+                desc: dept.description
             })
         } catch (err) {
             console.log(err)
@@ -23,14 +25,16 @@ class DepartmentController {
 
     getAllDepartment = async (req, res) => {
         try {
-            const { page, name, description } = req.query
-            const dept = await this.departmentService.getAllDepartment(page, name, description);
+            const { page, limit, text } = req.query
+            console.log(text)
+            const dept = await this.departmentService.getAllDepartment(page, limit, text);
             return res.status(200).json({
                 dept: dept.dept,
                 count: dept.count,
                 currentPage: dept.currentPage,
                 currentPageStart: dept.currentPageStart,
-                currentPageEnd: dept.currentPageEnd
+                currentPageEnd: dept.currentPageEnd,
+                currentLimit: dept.currentLimit
             })
         } catch (err) {
             console.log(err)
@@ -40,13 +44,13 @@ class DepartmentController {
 
     createDepartment = async (req, res) => {
         try {
-            const { name, description } = req.body;
-            if (!name && !description) {
+            const { name, desc } = req.body;
+            if (!name) {
                 return res.status(400).json({
                     error: 'Missing required fields.'
                 })
             }
-            const dept = await this.departmentService.createDepartment(name, description);
+            const dept = await this.departmentService.createDepartment(name, desc);
             return res.status(200).json({
                 success: `Successfully created department: ${dept.name} id: ${dept.id}`
             })
@@ -68,7 +72,9 @@ class DepartmentController {
             const dept = await this.departmentService.updateDepartment(id, name, description);
             return res.status(200).json({
                 success: `Successfully updated department: ${dept.name} id: ${dept.id}`,
-                dept: dept
+                id: dept.id,
+                name: dept.name,
+                desc: dept.description
             })
         } catch (err) {
             console.log(err)
@@ -88,6 +94,31 @@ class DepartmentController {
             return res.status(200).json({
                 success: `Successfully deleted department: ${dept.name} id: ${dept.id}`,
                 dept: dept
+            })
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({ error: err })
+        }
+    }
+
+    batchDeleteDepartment = async (req, res) => {
+        try {
+            const { id } = req.query
+            const dept = await this.departmentService.batchDeleteDepartment(id);
+            return res.status(200).json({
+                success: 'Successfully batch deleted department records.'
+            })
+        } catch {
+            console.log(err)
+            return res.status(500).json({ error: err })
+        }
+    }
+
+    getDepartmentCount = async (req, res) => {
+        try {
+            const departmentCount = await this.departmentService.getDepartmentCount();
+            return res.status(200).json({
+                departmentCount: departmentCount
             })
         } catch (err) {
             console.log(err)
