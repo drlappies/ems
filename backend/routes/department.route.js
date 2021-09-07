2ed2249dec3bb0d935ed036a6057b7f66c91df3a
drlappies <yau1chun2lap3@gmail.com>
2021-09-08 00:50:22 +0800

    route for batch delete

--- backend/routes/department.route.js
+++ backend/routes/department.route.js
@@ -3,9 +3,11 @@ const express = require('express');
 module.exports.DepartmentRoute = (departmentController) => {
     const router = express.Router();
     router.get('/', departmentController.getAllDepartment);
+    router.get('/metric', departmentController.getDepartmentCount);
     router.get('/:id', departmentController.getDepartment);
     router.post('/', departmentController.createDepartment);
     router.put('/:id', departmentController.updateDepartment);
     router.delete('/:id', departmentController.deleteDepartment);
+    router.delete('/', departmentController.batchDeleteDepartment);
     return router
 }
