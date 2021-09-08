c2f1e06b884fc90f9c98ea23d6a6d78c93edc9d8
drlappies <yau1chun2lap3@gmail.com>
2021-09-09 07:28:08 +0800

    added routes for batch delete and update

--- backend/routes/reimbursement.route.js
+++ backend/routes/reimbursement.route.js
@@ -3,8 +3,12 @@ const express = require('express');
 module.exports.ReimbursementRoute = (ReimbursementController) => {
     const router = express.Router();
     router.post('/', ReimbursementController.createReimbursement);
+    router.get('/metric', ReimbursementController.getReimbursementCount);
     router.put('/:id', ReimbursementController.updateReimbursement)
     router.get('/', ReimbursementController.getAllReimbursement);
     router.get('/:id', ReimbursementController.getReimbursement);
+    router.delete('/:id', ReimbursementController.deleteReimbursement);
+    router.put("/", ReimbursementController.batchUpdateReimbursement);
+    router.delete('/', ReimbursementController.batchDeleteReimbursement)
     return router
 }
