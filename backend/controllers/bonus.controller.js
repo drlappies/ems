bb1b84441c05889cfc29835835dafea415bc5a5e
drlappies <yau1chun2lap3@gmail.com>
2021-09-09 22:58:09 +0800

    added batch update/delete

--- backend/controllers/bonus.controller.js
+++ backend/controllers/bonus.controller.js
@@ -55,7 +55,6 @@ class BonusController {
             const bonus = await this.BonusService.editBonus(id, employeeId, reason, amount, date);
             return res.status(200).json({
                 success: `Successfully edited bouns ${bonus.id}`,
-                bonus: bonus
             })
         } catch (err) {
             console.log(err)
@@ -67,15 +66,16 @@ class BonusController {
 
     getAllBonus = async (req, res) => {
         try {
-            const { page, dateFrom, dateTo, amountFrom, amountTo, text } = req.query
-            const bonus = await this.BonusService.getAllBonus(page, dateFrom, dateTo, amountFrom, amountTo, text);
+            const { page, limit, dateFrom, dateTo, amountFrom, amountTo, text } = req.query
+            const bonus = await this.BonusService.getAllBonus(page, limit, dateFrom, dateTo, amountFrom, amountTo, text);
             return res.status(200).json({
                 bonus: bonus.bonus,
                 employee: bonus.employee,
                 currentPage: bonus.currentPage,
                 currentPageStart: bonus.currentPageStart,
                 currentPageEnd: bonus.currentPageEnd,
-                pageLength: bonus.pageLength
+                pageLength: bonus.pageLength,
+                currentLimit: bonus.currentLimit
             })
         } catch (err) {
             console.log(err)
@@ -96,6 +96,36 @@ class BonusController {
             const bonus = await this.BonusService.getBonus(id);
             return res.status(200).json({
                 bonus: bonus,
+            })
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({
+                error: err
+            })
+        }
+    }
+
+    batchUpdateBonus = async (req, res) => {
+        try {
+            const { id, employee_id, date, reason, amount } = req.body
+            const bonus = await this.BonusService.batchUpdateBonus(id, employee_id, date, reason, amount)
+            return res.status(200).json({
+                success: 'Successfully batch updated bonus records.'
+            })
+        } catch (err) {
+            console.log(err)
+            return res.status(500).json({
+                error: err
+            })
+        }
+    }
+
+    batchDeleteBonus = async (req, res) => {
+        try {
+            const { id } = req.query
+            const bonus = await this.BonusService.batchDeleteBonus(id)
+            return res.status(200).json({
+                success: 'Successfully batch deleted bonus records.'
             })
         } catch (err) {
             console.log(err)
--- backend/services/bonus.service.js
+++ backend/services/bonus.service.js
@@ -19,19 +19,24 @@ class BonusService {
     }
 
     editBonus = async (id, employeeId, reason, amount, date) => {
-        const [bonus] = await this.knex('bonus').where('id', id).update({
-            employee_id: employeeId,
-            reason: reason,
-            amount: amount,
-            date: date
-        }, ['id', 'reason', 'amount', 'date'])
+        const [bonus] = await this.knex('bonus')
+            .where('id', id)
+            .update({
+                employee_id: employeeId,
+                reason: reason,
+                amount: amount,
+                date: date
+            }, ['id'])
         return bonus
     }
 
-    getAllBonus = async (page, dateFrom, dateTo, amountFrom, amountTo, text) => {
+    getAllBonus = async (page, limit, dateFrom, dateTo, amountFrom, amountTo, text) => {
+        if (!page || page < 0) page = 0;
+        if (!limit || limit < 0) limit = 10;
         let currentPage = parseInt(page)
         let currentPageStart = parseInt(page) + 1
-        let currentPageEnd = parseInt(page) + 15
+        let currentPageEnd = parseInt(page) + parseInt(limit)
+        let currentLimit = parseInt(limit)
 
         const [count] = await this.knex
             .count()
@@ -63,7 +68,7 @@ class BonusService {
         const bonus = await this.knex('bonus')
             .join('employee', 'bonus.employee_id', 'employee.id')
             .select(['bonus.id', 'bonus.employee_id', 'employee.firstname', 'employee.lastname', 'bonus.date', 'bonus.reason', 'bonus.amount'])
-            .limit(15)
+            .limit(currentLimit)
             .offset(currentPage)
             .orderBy('bonus.id')
             .modify((queryBuilder) => {
@@ -91,7 +96,7 @@ class BonusService {
             currentPageEnd = parseInt(count.count)
         }
 
-        return { bonus: bonus, employee: employee, currentPage: currentPage, currentPageStart: currentPageStart, currentPageEnd: currentPageEnd, pageLength: count.count }
+        return { bonus: bonus, employee: employee, currentPage: currentPage, currentPageStart: currentPageStart, currentPageEnd: currentPageEnd, pageLength: count.count, currentLimit: currentLimit }
     }
 
     getBonus = async (id) => {
@@ -100,6 +105,26 @@ class BonusService {
             .select(['bonus.employee_id', 'bonus.id', 'bonus.reason', 'bonus.amount', 'bonus.date', 'employee.firstname', 'employee.lastname'])
             .where('bonus.id', id)
 
+        return bonus
+    }
+
+    batchUpdateBonus = async (id, employee_id, date, reason, amount) => {
+        if (!Array.isArray(id)) id = [id]
+
+        let update = {};
+        if (employee_id) update.employee_id = employee_id;
+        if (date) update.date = date;
+        if (reason) update.reason = reason;
+        if (amount) update.amount = amount;
+
+        const bonus = await this.knex('bonus').whereIn('id', id).update(update)
+
+        return bonus
+    }
+
+    batchDeleteBonus = async (id) => {
+        if (!Array.isArray(id)) id = [id]
+        const bonus = await this.knex('bonus').whereIn('id', id).del()
         return bonus
     }
 }
