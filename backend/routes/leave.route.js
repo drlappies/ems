b91642c8ad47271230008e9eeda0df8748c5fc4a
drlappies <yau1chun2lap3@gmail.com>
2021-09-10 02:59:27 +0800

    added batch update/delete routes for leave

--- backend/routes/leave.route.js
+++ backend/routes/leave.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 module.exports.LeaveRoute = (LeaveController) => {
     const router = express.Router();
-    router.get('/rate', LeaveController.leaveRate)
+    router.get('/metric', LeaveController.leaveRate)
     router.get('/', LeaveController.getAllLeave)
     router.get('/:id', LeaveController.getLeave)
     router.post('/', LeaveController.createLeave)
-    router.put('/:id', LeaveController.approveLeave)
+    router.put('/', LeaveController.updateLeave)
+    router.delete('/', LeaveController.deleteLeave)
     return router
 }
