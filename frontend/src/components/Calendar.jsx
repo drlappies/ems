c6d012b798c5742a2ebad0295fc2de1816ada775
drlappies <yau1chun2lap3@gmail.com>
2021-09-11 14:25:38 +0800

    user dashboard with attendance calendar review

---
+++ frontend/src/components/Calendar.jsx
@@ -0,0 +1,170 @@
+import React, { useMemo, useState, useEffect, useCallback } from 'react'
+import { useSelector } from 'react-redux'
+import { Button } from 'semantic-ui-react'
+import axios from 'axios'
+import '../css/main.css'
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const generateCalendar = (currentYear, currentMonth, monthlyAttendance) => {
+    const start = new Date(currentYear, currentMonth, 1);
+    const end = new Date(currentYear, currentMonth + 1, 0);
+    let month = [];
+    for (let i = 0; i < 6; i++) {
+        if (start > end) break;
+        let week = [];
+        for (let j = 0; j < 7; j++) {
+            if (j !== start.getDay() || start > end) {
+                week.push(null)
+            } else if (monthlyAttendance.find(el => new Date(el.date).getDate() === start.getDate())) {
+                const attendance = monthlyAttendance.find(el => new Date(el.date).getDate() === start.getDate())
+                start.setDate(start.getDate() + 1)
+                week.push({
+                    date: new Date(attendance.date).getDate(),
+                    status: attendance.status,
+                    checkIn: attendance.check_in,
+                    checkOut: attendance.check_out,
+                })
+            } else {
+                week.push({
+                    date: start.getDate(),
+                    status: null,
+                    checkIn: null,
+                    checkOut: null
+                })
+                start.setDate(start.getDate() + 1)
+            }
+        }
+        month.push(week)
+    }
+
+    return month
+}
+
+const yearSelect = (currentYear) => {
+    let years = [];
+    const earliestYear = 1970;
+    for (let i = currentYear; i > earliestYear; i--) {
+        years.push(i)
+    }
+    return years
+}
+
+function Calendar(props) {
+    const auth = useSelector(state => state.auth)
+    const [state, setState] = useState({
+        currentMonth: new Date().getMonth(),
+        currentYear: new Date().getFullYear(),
+        currentMonthlyAttendance: [],
+    })
+
+    const calendar = useMemo(() => generateCalendar(state.currentYear, state.currentMonth, state.currentMonthlyAttendance), [state.currentMonth, state.currentYear, state.currentMonthlyAttendance])
+    const years = yearSelect(new Date().getFullYear())
+
+    const fetchMonthlyAttendance = useCallback(async () => {
+        const res = await axios.get(`/attendance/record/${auth.id}`, {
+            params: {
+                month: state.currentMonth,
+                year: state.currentYear
+            }
+        })
+        setState(prevState => {
+            return {
+                ...prevState,
+                currentMonthlyAttendance: res.data.attendance,
+            }
+        })
+
+    }, [auth.id, state.currentMonth, state.currentYear])
+
+    useEffect(() => {
+        fetchMonthlyAttendance()
+    }, [fetchMonthlyAttendance, state.currentMonth, state.currentYear])
+
+    const next = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                currentMonth: prevState.currentMonth + 1 > 11 ? 0 : prevState.currentMonth + 1,
+                currentYear: prevState.currentMonth + 1 > 11 ? prevState.currentYear + 1 : prevState.currentYear
+            }
+        })
+    }
+
+    const prev = () => {
+        setState(prevState => {
+            return {
+                ...prevState,
+                currentMonth: prevState.currentMonth - 1 <= 0 ? 11 : prevState.currentMonth - 1,
+                currentYear: prevState.currentMonth - 1 <= 0 ? prevState.currentYear - 1 : prevState.currentYear
+            }
+        })
+    }
+
+    const jump = (e) => {
+        const { name, value } = e.target;
+        setState(prevState => {
+            return {
+                ...prevState,
+                [name]: parseInt(value)
+            }
+        })
+    }
+
+    return (
+        <div >
+            <Button size="tiny" onClick={() => prev()}>Last Month</Button>
+            <Button size="tiny" onClick={() => next()}>Next Month</Button>
+            <span>Jump To:
+                <select name="currentMonth" onChange={(e) => jump(e)} defaultValue={state.currentMonth}>{months.map((el, i) =>
+                    <option key={i} value={i}>{el}</option>)}
+                </select>
+                <select name="currentYear" onChange={(e) => jump(e)} defaultValue={state.currentYear}>
+                    {years.map((el, i) =>
+                        <option key={i} value={el}>{el}</option>
+                    )}
+                </select>
+            </span>
+            <div className="calendar-container">
+                <table className="calendar">
+                    <thead>
+                        <tr>
+                            <th colSpan="7">{state.currentYear} {months[state.currentMonth]}</th>
+                        </tr>
+                    </thead>
+                    <thead>
+                        <tr>
+                            <th>Sun</th>
+                            <th>Mon</th>
+                            <th>Tue</th>
+                            <th>Wed</th>
+                            <th>Thu</th>
+                            <th>Fri</th>
+                            <th>Sat</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {calendar.map((week, i) =>
+                            <tr key={i}>
+                                {week.map((date, j) =>
+                                    <td className="calendar-date" key={j}>
+                                        {date ?
+                                            <div>
+                                                {date.checkIn ? <div>In: {date.checkIn}</div> : null}
+                                                {date.checkOut ? <div>Out: {date.checkOut}</div> : null}
+                                                {date.status ? <div style={{ backgroundColor: date.status === 'on_time' ? 'green' : 'red' }}>{date.status}</div> : null}
+                                                <div>{date.date}</div>
+                                            </div>
+                                            : null}
+                                    </td>
+                                )}
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Calendar
---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Grid, Segment, Header, Table, Tab } from 'semantic-ui-react'
+import Calendar from './Calendar';
+import '../css/main.css'
+
+function User() {
+    const auth = useSelector(state => state.auth)
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const panes = [
+        { menuItem: 'Attendance Record', render: () => <Tab.Pane><Calendar /></Tab.Pane> },
+        { menuItem: 'Leave Record', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+        { menuItem: 'Payroll Record', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+        { menuItem: 'Reimbursement Record', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+        { menuItem: 'Allowance Record', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    ]
+
+    return (
+        <div className="record">
+            <Header>User Dashboard</Header>
+            <Grid>
+                <Grid.Row columns="2">
+                    <Grid.Column width="6">
+                        <Segment>
+                            <Segment.Inline>
+                                <Header as="h4">Employee Profile</Header>
+                            </Segment.Inline>
+                            <Table size="small">
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Employee ID</Header></Table.Cell>
+                                    <Table.Cell>{auth.id}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Firstname</Header></Table.Cell>
+                                    <Table.Cell>{auth.firstname}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Lastname</Header></Table.Cell>
+                                    <Table.Cell>{auth.lastname}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Account Role</Header></Table.Cell>
+                                    <Table.Cell>{auth.role}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Department</Header></Table.Cell>
+                                    <Table.Cell>{auth.department ? auth.department : "unassigned"}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Position</Header></Table.Cell>
+                                    <Table.Cell>{auth.position ? auth.position : "unassigned"}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Working Hour</Header></Table.Cell>
+                                    <Table.Cell>{auth.start_hour} to {auth.end_hour}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Address</Header></Table.Cell>
+                                    <Table.Cell>{auth.address}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Phone Number</Header></Table.Cell>
+                                    <Table.Cell>{auth.phone_number}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Onboard Date</Header></Table.Cell>
+                                    <Table.Cell>{new Date(auth.onboard_date).getDate()} {months[new Date(auth.onboard_date).getMonth()]}  {new Date(auth.onboard_date).getFullYear()} </Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Annual Leave</Header></Table.Cell>
+                                    <Table.Cell>{auth.annual_leave_count}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Overtime Pay Entitlement</Header></Table.Cell>
+                                    <Table.Cell>{auth.ot_pay_entitled ? "Yes" : "No"}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Overtime Pay (Hourly)</Header></Table.Cell>
+                                    <Table.Cell>{auth.ot_hourly_salary}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Emergency Contact Person</Header></Table.Cell>
+                                    <Table.Cell>{auth.emergency_contact_person}</Table.Cell>
+                                </Table.Row>
+                                <Table.Row>
+                                    <Table.Cell collapsing><Header as="h5">Emergency Contact Number</Header></Table.Cell>
+                                    <Table.Cell>{auth.emergency_contact_number}</Table.Cell>
+                                </Table.Row>
+                            </Table>
+                        </Segment>
+                    </Grid.Column>
+                    <Grid.Column width="10">
+                        <Tab panes={panes} />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div >
+    )
+}
+
+export default User
