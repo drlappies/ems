89a40d7a4961e4fec8e08f072111c14462d80258
drlappies <yau1chun2lap3@gmail.com>
2022-06-17 02:56:44 +0800

    add role/status verify in user middleware

--- backend/middlewares/user.middleware.js
+++ backend/middlewares/user.middleware.js
@@ -18,6 +18,52 @@ class UserMiddleware {
             res.status(500).json(error)
         }
     }
+
+    verifyStatus = async (req, res, next) => {
+        try {
+            let user = req.user
+
+            if (!user) {
+                const token = req.headers.token
+                const jwtRes = await this.jwt.verify(token, process.env.SECRET)
+
+                user = await this.services.employee.getOneById(jwtRes.id)
+                req.user = user
+            }
+
+            if (user.status === "unavailable") {
+                res.status(400).json({ error: "user is unavailable/banned" })
+                return
+            }
+
+            next()
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
+    verifyRole = async (req, res, next) => {
+        try {
+            let user = req.user
+
+            if (!user) {
+                const token = req.headers.token
+                const jwtRes = await this.jwt.verify(token, process.env.SECRET)
+
+                user = await this.services.employee.getOneById(jwtRes.id)
+                req.user = user
+            }
+
+            if (user.role === "employee") {
+                res.status(400).json({ error: "no permission" })
+                return
+            }
+
+            next()
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
 }
 
 export default UserMiddleware
